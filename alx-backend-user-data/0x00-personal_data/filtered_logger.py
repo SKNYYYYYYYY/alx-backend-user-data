bfb4d1dd1a553c9b05a6e8d5357bcddd23aded33
Newton <123421090+SKNYYYYYYYY@users.noreply.github.com>
2025-05-21 21:09:02 +0300

    Delete alx-backend-user-data directory

--- alx-backend-user-data/0x00-personal_data/filtered_logger.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""filters PII in a log message"""
-import re
-
-
-def filter_datum(fields, redaction, message, separator):
-	"""obfuscate strings in fields"""
-	pattern = r'(' + '|'.join(re.escape(field) for field in fields) + r')=([^;]+)'
-	return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
