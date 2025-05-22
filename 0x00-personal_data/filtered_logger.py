52dacf78dfc5d96e6b427621b6ff69e6128f6abf
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-22 20:51:40 +0300

    type annote filter_datum main

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,17 @@
-#!usr/bin/env python3
-"""Filtering log messages"""
+#!/usr/bin/env python3
+"""
+Module that contains filter_datum function to obfuscate specified fields in a log message.
+"""
+
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator) -> str:
-    """use regex to mask specified field's value"""
-    pattern = r'(' + '|'.join(re.escape(field)
-                              for field in fields) + r')=([^;]+)'
-    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
+    """Returns the log message with specified fields obfuscated using regex."""
+    return re.sub(rf'({"|".join(fields)})=.*?{separator}',
+                  lambda m: f"{m.group(1)}={redaction}{separator}", message)
