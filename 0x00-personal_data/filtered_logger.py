f0806f2d2029d6ffeb70127a85c13ddbef573931
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-22 20:47:56 +0300

    obfuscate log message main

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-	"""use regex to mask specified field's value"""
-	pattern = r'(' + '|'.join(re.escape(field) for field in fields) + r')=([^;]+)'
-	return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+    """use regex to mask specified field's value"""
+    pattern = r'(' + '|'.join(re.escape(field)
+                              for field in fields) + r')=([^;]+)'
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
