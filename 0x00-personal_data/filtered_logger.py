6589e8984f1330b706d2657ef6065f49f2e9927a
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-22 20:55:29 +0300

    type annote filter_datum main

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """
-Module that contains filter_datum function to obfuscate specified fields in a log message.
+contains filter_datum function to obfuscate specified fields in a log message.
 """
 import re
 from typing import List
 
 
-def filter_datum(fields,redaction, message,separator) -> str:
+def filter_datum(fields, redaction, message, separator) -> str:
     """Returns the log message with specified fields obfuscated using regex."""
     return re.sub(rf'({"|".join(fields)})=.*?{separator}',
                   lambda m: f"{m.group(1)}={redaction}{separator}", message)
