596b55b92b5a84ad01fefc738591e9452be4b292
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-24 16:50:55 +0300

    encrypt a password and return its hash main

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,7 +3,7 @@
 import bcrypt
 
 
-def hash_password(password) -> bytes:
+def hash_password(password: str) -> bytes:
     """Hash a password for the first time, with a randomly-generated salt"""
     hashed = bcrypt.hashpw(bytes(password, "utf-8"), bcrypt.gensalt())
     return hashed
