842fccb88c7266dfece45dfc281103d41c2f06ba
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-24 14:10:26 +0300

    create a custom filter main

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Contains filter_datum function to obfuscate specified fields in a log message.
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -11,3 +12,24 @@ def filter_datum(fields: List[str], redaction: str,
     """Returns the log message with specified fields obfuscated using regex."""
     return re.sub(rf'({"|".join(fields)})=.*?{separator}',
                   lambda m: f"{m.group(1)}={redaction}{separator}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """custom formater"""
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(levelname)s %(asctime)s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Constructor"""
+        self.fields = fields
+        super().__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """redact pii"""
+        record.msg = filter_datum(
+            self.fields,
+            self.REDACTION,
+            record.getMessage(),
+            self.SEPARATOR)
+        return super().format(record)
