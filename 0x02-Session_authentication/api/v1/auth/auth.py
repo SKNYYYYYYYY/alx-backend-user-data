b8f271b3d2535f7e502249d8833a7929594dd716
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-29 20:49:09 +0300

    session authentication main

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Authentication module
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """class to manage API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """return False"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        path = path.rstrip('/') + '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """return None"""
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """return None"""
+        return None
