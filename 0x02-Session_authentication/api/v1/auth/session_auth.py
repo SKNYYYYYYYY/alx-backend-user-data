24536b2af530dd75c1e6304403f5425dbf34b717
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-30 01:22:03 +0300

    handle all routes for the Session authentication main

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,10 @@
 from api.v1.auth.auth import Auth
 from api.v1.views.users import User
 import uuid
+from api.v1.views import app_views
+from flask import request, jsonify
+import os
+
 
 
 class SessionAuth(Auth):
@@ -34,3 +38,28 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(session_id)
         user = User.get(user_id)
         return user
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes = False)
+def login():
+    """login handler"""
+    email = request.form.get('email')
+    if not email:
+        return  { "error": "email missing" }, 400
+    password = request.form.get('password')
+    if not password:
+        return  { "error": "password missing" }, 400
+    User.load_from_file()
+    users = User.search({"email": email})
+    if users is None:
+        return { "error": "no user found for this email" }, 404
+    for user in users:
+        if not user.is_valid_password(password):
+            return { "error": "wrong password" }, 401
+        else:
+            from api.v1.app import auth
+            user_id = user.id
+            session_id = auth.create_session(user_id)
+            cookie_name =os.getenv('SESSION_NAME')
+            out = jsonify(user.to_json())
+            out.set_cookie(cookie_name, session_id)
+            return out
