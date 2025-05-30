217dc6a8bd5cf1661ffc365a751c09a15d661400
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-30 18:10:29 +0300

    corrected pycodestyle errors main

--- 0x02-Session_authentication/main_1.py
+++ 0x02-Session_authentication/main_1.py
@@ -13,4 +13,3 @@ if __name__ == "__main__":
         f.write(f'curl "http://0.0.0.0:5000/api/v1/users/me" --cookie "_my_session_id=fe3d2f81-ce45-43b4-991d-e28ed153b1be"' )
     f.close()
 
-curl "http://0.0.0.0:5000/api/v1/users/me" --cookie "_my_session_id=8c4186b4-9e97-41f2-8d30-d599bfa83b19"
