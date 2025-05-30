80df744f89b989b344a26df2c5861d2f9585134f
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-05-30 21:11:01 +0300

    Allow * at the end of excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,9 +12,15 @@ class Auth():
         """return False"""
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
-        path = path.rstrip('/') + '/'
-        if path in excluded_paths:
-            return False
+        if path.startswith("*"):
+            path_start = path.rstrip("*")
+            for path in excluded_paths:
+                if (path.startswith(path_start)):
+                    return False
+        else:
+            path = path.rstrip('/') + '/'
+            if path in excluded_paths:
+                return False
         return True
 
     def authorization_header(self, request=None) -> str:
