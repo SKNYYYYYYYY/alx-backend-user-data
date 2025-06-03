a7bb91d5adbb41ad9e984cd1a5d37291f04e30bc
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-03 19:15:16 +0300

    implement finding userin the db using User attributes main

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Database module
+"""
+from requests import session
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+    def __init__(self) -> None:
+        """constructor"""
+        self._engine = create_engine('sqlite:///a.db', echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object"""
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """save a user to the db"""
+        user = User(email=email, hashed_password=hashed_password)
+        self._session
+        self.__session.add(user)
+        self.__session.commit()
+        return user
+
+    def find_user_by(self, **kwargs):
+        """returns a user from the db"""
+        self._session
+        for key, val in kwargs.items():
+            try:
+                getattr(User, key)
+            except Exception:
+                raise InvalidRequestError
+            user = self.__session.query(User).filter(getattr(User, key) == val)
+            if user.count() == 0:
+                raise NoResultFound
+            return user.first()
---
+++ 0x03-user_authentication_service/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from user import User
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
+
+my_db = DB()
+
+user_1 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+user_1 = my_db.add_user("test1@test.com", "AdvHashedPwd1")
+# print(user_1.id)
+user_2 = my_db.add_user("test2@test.com", "BasicHashedPwd1")
+# print(user_2.id)
+try:
+    find_user = my_db.find_user_by(no_email="tests@test.com")
+    print(find_user.id)
+except InvalidRequestError:
+    print("Invalid")
