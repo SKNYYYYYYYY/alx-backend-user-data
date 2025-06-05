1b0add580c82612c48a84203ddeb720bb2e87d68
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-05 12:17:05 +0300

    Implement generate reset token main

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,8 +80,8 @@ class Auth:
         """gernerates a UUID as reset token"""
         try:
             user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
         except Exception:
             raise ValueError
-        reset_token = _generate_uuid()
-        self._db.update_user(user.id, reset_token=reset_token)
-        return reset_token
