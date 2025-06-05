2525235c5cc0d9d266f0fbd70705ccad48ec6f29
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-05 12:12:12 +0300

    Implement generate reset token main

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,7 +81,8 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except Exception:
-            raise ValueError("User Not Found")
+            raise ValueError("User not found")
+
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
