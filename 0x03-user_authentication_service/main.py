b6079e0227cd66c02fb091a5b212e0eec3841fc4
SKNYYYYYYYY <newtonsigei13105@gmail.com>
2025-06-07 00:03:44 +0300

    feat: create functions to test the endpoints

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,24 +1,103 @@
-#!/usr/bin/env python3
-"""
-Main file
-"""
-from auth import Auth
-from db import DB
+import requests
 
-email = 'bob@bob.com'
-password = 'mySuperPwd'
-auth = Auth()
-db = DB()
+BASE_URL = "http://localhost:5000"
 
 
-auth.register_user(email, password)
-ses = (auth.create_session(email))
+def register_user(email: str, password: str) -> None:
+    """Registers a new user."""
+    url = f"{BASE_URL}/users"
+    payload = {"email": email, "password": password}
+    response = requests.post(url, data=payload)
 
-user = auth.get_user_from_session_id(session_id=ses)
-tokn = user.reset_token
+    assert response.status_code == 200, f"Expected 200, got {
+        response.status_code}"
+    assert response.json() == {
+        "email": email, "message": "user created"}, f"Unexpected payload: {
+        response.json()}"
 
-tokn = auth.get_reset_password_token(email)
 
-print((tokn))
-# print(auth.create_session("unknown@email.com"))
+def log_in_wrong_password(email: str, password: str) -> None:
+    """Tries to log in with a wrong password."""
+    url = f"{BASE_URL}/sessions"
+    payload = {"email": email, "password": password}
+    response = requests.post(url, data=payload)
 
+    assert response.status_code == 401, f"Expected 401, got {
+        response.status_code}"
+
+
+def log_in(email: str, password: str) -> str:
+    """log in"""
+    url = f"{BASE_URL}/sessions"
+    payload = {"email": email, "password": password}
+    response = requests.post(url, data=payload)
+
+    assert response.status_code == 200, f"Expected 401, got {
+        response.status_code}"
+    return response.cookies.get("session_id")
+
+
+def profile_unlogged() -> None:
+    """profile without session id"""
+    url = f"{BASE_URL}/profile"
+    response = requests.get(url)
+    assert response.status_code == 403, f"Expected 403, got {
+        response.status_code}"
+
+
+def profile_logged(session_id: str) -> None:
+    """profile with session id"""
+    url = f"{BASE_URL}/profile"
+    payload = {"session_id": session_id}
+    response = requests.get(url, cookies=payload)
+    assert response.status_code == 200, f"Expected 200, got {
+        response.status_code}"
+
+
+def log_out(session_id: str) -> None:
+    """logout with session id"""
+    url = f"{BASE_URL}/sessions"
+    payload = {"session_id": session_id}
+    response = requests.delete(url, cookies=payload)
+    assert response.status_code == 200, f"Expected 200, got {
+        response.status_code}"
+
+
+def reset_password_token(email: str) -> str:
+    """reset password tokken"""
+    url = f"{BASE_URL}/reset_password"
+    payload = {"email": email}
+    response = requests.post(url, data=payload)
+    assert response.status_code == 200, f"Expected 200, got {
+        response.status_code}"
+    reset_token = response.json()
+    return reset_token.get("reset_token")
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """update password"""
+    url = f"{BASE_URL}/reset_password"
+    payload = {
+        "email": email,
+        "reset_token": reset_token,
+        "new_password": new_password}
+    response = requests.put(url, data=payload)
+    assert response.status_code == 200, f"Expected 200, got {
+        response.status_code}"
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
